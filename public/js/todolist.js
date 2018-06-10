c7d58ad46682276a6959c5fefebc64362b1f29f5
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-06-10 23:25:14 +0800

    no message

--- public/js/todolist.js
+++ public/js/todolist.js
@@ -1,3 +1,44 @@
-"use strict";
+'use strict';
 
-console.log(123);
+var mandy = mandy || {};
+
+mandy.public = function ($) {
+  var d = new Date(),
+      $inputfile = $('[data=check-file]'),
+      init = function init() {
+    console.log(123);
+    $inputfile.on('change', 'input', function () {
+      $(this).parent().find('.fileinfo').empty();
+      $(this).after(function () {
+        var d = new Date(),
+            date = d,
+            temp = [];
+
+        temp.push('<div class="fileinfo">', '<p class="name">', $(this).val(), '</p>', '<p class="date">', date, '</p>', '</div>');
+        return temp.join('');
+      });
+      console.log($(this).val());
+    });
+  };
+  return {
+    init: init
+  };
+}(jQuery);
+
+mandy.addTask = function ($) {
+  var init = function init() {
+    $('.add').on('click', function () {
+      $(this).parent().addClass('is-active');
+    });
+    $('.save,.cancel').on('click', function () {
+      $(this).parents('.m-addtask').removeClass('is-active');
+    });
+    console.log(456);
+  };
+  return {
+    init: init
+  };
+}(jQuery);
+
+mandy.public.init();
+mandy.addTask.init();
