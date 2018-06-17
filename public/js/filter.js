a31a2a6d2ca7f74d42c33252a985c128d1e1061b
Ｍandy <mandy@andyde-MacBook-Pro.local>
2018-06-18 00:36:32 +0800

    Week2  filter

---
+++ public/js/filter.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var mandy = mandy || {};
+mandy.model = mandy.model || {};
+mandy.model.filter = function ($) {}(jQuery);
+
+mandy.view = mandy.view || {};
+mandy.view.filter = function ($) {}(jQuery);
+
+mandy.controller = mandy.controller || {};
+mandy.controller.filter = function ($) {}(jQuery);
+
+mandy.view.filter;
---
+++ sourse/js/filter.js
@@ -0,0 +1,22 @@
+
+var mandy = mandy || {};
+mandy.model = mandy.model || {};
+mandy.model.filter = (function($){
+ 
+
+})(jQuery);
+
+
+
+mandy.view = mandy.view || {};
+mandy.view.filter = (function($){
+  
+})(jQuery);
+
+mandy.controller = mandy.controller || {};
+mandy.controller.filter = (function($){
+
+})(jQuery);
+
+
+mandy.view.filter;
