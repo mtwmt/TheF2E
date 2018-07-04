c147866a39351e70fa7f962d6300bacfc87ffc02
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-07-05 01:45:16 +0800

    add week5

---
+++ public/js/comicviewer.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var mandy = mandy || {};
+mandy.model = mandy.model || {};
+mandy.model.comicviewer = function ($) {
+  // var
+
+  // return {
+  // }
+
+}(jQuery);
+
+mandy.view = mandy.view || {};
+mandy.view.comicviewer = function ($) {
+  var init = function init(data) {};
+  return {
+    init: init
+  };
+}(jQuery);
+
+mandy.controller = mandy.controller || {};
+mandy.controller.comicviewer = function ($) {
+  var init = function init() {};
+  return {
+    init: init
+  };
+}(jQuery);
+
+mandy.controller.comicviewer.init();
---
+++ sourse/js/comicviewer.js
@@ -0,0 +1,40 @@
+
+var mandy = mandy || {};
+mandy.model = mandy.model || {};
+mandy.model.comicviewer = (function($){
+  // var
+  
+  // return {
+  // }
+
+})(jQuery);
+
+
+
+mandy.view = mandy.view || {};
+mandy.view.comicviewer = (function($){
+  var 
+  init = function( data ){
+    
+  };
+  return{
+    init: init
+  }
+    
+})(jQuery);
+
+mandy.controller = mandy.controller || {};
+mandy.controller.comicviewer = (function($){
+  var
+
+  init = function(){
+    
+  };
+  return{
+    init: init
+  }
+})(jQuery);
+
+
+mandy.controller.comicviewer.init();
+
