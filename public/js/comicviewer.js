cc7075537feec6a0dc83c98f0852ad54f11aa8d6
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-09-04 00:53:21 +0800

    comicviewer img onload OK !

--- public/js/comicviewer.js
+++ public/js/comicviewer.js
@@ -2,53 +2,21 @@
 
 var mandy = mandy || {};
 mandy.model = mandy.model || {};
+
 mandy.model.comicviewer = function ($) {
-  var imgLoad = function imgLoad(url, callback) {
-    var img = new Image();
-    img.src = url;
-    if (img.complete) {
-      // 如果圖片已經存在於瀏覽器緩存，直接調用回調函數 
-      callback.call(img);
-      return; // 直接返回，不用再處理onload事件 
-    }
-    img.onload = function () {
-      //圖片下載完畢時非同步調用callback函數。 
-      callback.call(img); //將回調函數的this替換為Image對象 
-    };
-  },
-      getData = function getData(obj, callback) {
-    var set = obj.data;
-    set.chapter = obj.data.chapter || 0;
-    set.pg = obj.data.pg || 0;
+  var getData = function getData(callback, obj) {
     $.ajax({
       url: '../json/comicviewer.json',
       method: 'get',
       dataType: 'json',
-      data: set,
+      data: {},
       success: function success(data) {
-        // data = data || {};
-
-        // data.comic.map(function( e,i ){
-        //   e.list.map(function( el,idx ){
-        //     imgLoad( el.url, function(){
-        //       var temp = [];
-        //       temp.width = el.width = this.width;
-        //       temp.height = el.height = this.height;
-        //       return temp;
-        //     });
-
-
-        //   });
-        //   console.log( data.comic[0].list[0].url )
-        // });
-
-
-        obj.success(data);
+        data = data || {};
+        callback(data);
       }
     });
   };
   return {
-    imgLoad: imgLoad,
     getData: getData
   };
 }(jQuery);
@@ -80,28 +48,10 @@ mandy.view.comicviewer = function ($) {
   },
       tempItem = function tempItem(data, chapter) {
     var temp = [];
-
     data[chapter].list.map(function (e, i) {
       temp.push('<li><img src="', e.url, '" width="', e.width, '" height="', e.height, '" alt=""></li>');
-
-      // console.log( e )
     });
-
-    return ['<div class="m-slide">', '<div class="items">', '<div class="btn-prev"></div>', '<div class="btn-next"></div>', '<ul>',
-    // temp.join(''),
-    // '<li class="active"><img src="images/comicviewer/storyboard-1.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-2.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-3.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-4.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-5.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-6.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-7.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-8.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-9.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-10.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-11.png" alt=""></li>',
-    // '<li><img src="images/comicviewer/storyboard-12.png" alt=""></li>',
-    '</ul>', '</div>', '</div>'].join('');
+    return ['<div class="m-slide">', '<div class="items">', '<div class="btn-prev"></div>', '<div class="btn-next"></div>', '<ul></ul>', '</div>', '</div>'].join('');
   },
       init = function init(data, chapter) {
     var $pageInner = $('page-inner > .content');
@@ -116,7 +66,7 @@ mandy.view.comicviewer = function ($) {
 }(jQuery);
 
 mandy.controller = mandy.controller || {};
-mandy.controller.comicviewer = function ($) {
+mandy.controller.comicviewer = function () {
   var $layout = $('page-inner > .content'),
       $page,
       $slide,
@@ -124,111 +74,93 @@ mandy.controller.comicviewer = function ($) {
       $item,
       $item_len,
       picidx,
-      creatPage = function creatPage(data) {
-    mandy.view.comicviewer.init(data, 0);
-
-    slide(data.comic, 0);
+      imgLoad = function imgLoad(url, idx) {
+    var img = new Image();
+    img.onload = function () {
+      slideEvent(img, idx);
+    };
+    img.src = url;
   },
-      imgChange = function imgChange(idx) {
-    $item.removeClass('active');
-    $item.eq(idx).addClass('active');
-    $slide.stop().animate({ scrollLeft: $item.eq(idx).data('pos') }, 400);
-    $page.find('img').attr('src', $item.eq(idx).find('img').attr('src'));
-  },
-      slideEvent = function slideEvent() {
+      slideEvent = function slideEvent(img, idx) {
+    var $item = $('<li />').append(img).attr('data-num', idx);
 
-    $item = $('<li />').append(this);
     $items.append($item);
-
     $slide.height($item.outerHeight() + 40);
-    $item_len = $item.length;
     $item.width(parseInt($slide.outerWidth() / 6, 10));
 
     var $item_w = $item.width(),
         $item_btn_prev = $slide.find('.btn-prev'),
-        $item_btn_next = $slide.find('.btn-next');
+        $item_btn_next = $slide.find('.btn-next'),
+        pos;
 
-    // 計算圖片位子 分別給值
-    // $item.each(function( i,e ){
-    //   var pos;
-    //   $(e).css({ left: i * $item_w }).attr('data-num', i);
-    //   if( i > 2 && i < $item_len - 3 ){
-    //     pos = (i-2) * $item_w;
-    //   }else if( i >= ($item_len - 3) ){
-    //     pos = ($item_len - 3) * $item_w;
-    //   }else{
-    //     pos = 0;
-    //   }
-    //   $(e).attr('data-pos', pos );
-    // });
-    // $item_btn_prev.width( $item.find('img').outerWidth() / 2 ).height( $item.find('img').height() );
-    // $item_btn_next.width( $item.find('img').outerWidth() / 2 ).height( $item.find('img').height() );
+    $item.css({ left: idx * $item_w });
 
-    // $items.css({ left: $item_w / 2 });
-    // $items.width( $item_len * $item_w + $item_w / 2 );
-    // picidx = $items.find('.active').data('num');
+    if (idx > 2 && idx < $item_len - 3) {
+      pos = (idx - 2) * $item_w;
+    } else if (idx >= $item_len - 3) {
+      pos = ($item_len - 3) * $item_w;
+    } else {
+      pos = 0;
+    }
+    $item.attr('data-pos', pos);
+    $items.css({ left: $item_w / 2 });
+    $items.width($item_len * $item_w + $item_w / 2);
+    $item_btn_prev.width($item.find('img').outerWidth() / 2).height($item.find('img').height());
+    $item_btn_next.width($item.find('img').outerWidth() / 2).height($item.find('img').height());
 
-
-    // // console.log( $item.find('img').parents('.items').find('li').width() )
-
-    // $item.on('click',function(){
-    //   picidx = $(this).data('num');
-    //   imgChange( picidx );
-    // });
-    // $item_btn_prev.on('click',function(){
-    //   if( picidx === 0 ){
-    //     picidx = 0;
-    //   }else{
-    //     picidx = picidx - 1;
-    //   }
-    //   imgChange( picidx );
-    // });
-    // $item_btn_next.on('click',function(){
-    //   if( picidx === $item_len - 1){
-    //     picidx = picidx;
-    //   }else{
-    //     picidx = picidx + 1;
-    //   }
-    //   imgChange( picidx );
-    // });
+    $items.find('[data-num=' + picidx + ']').trigger('click');
   },
-      slide = function slide(data, chapter) {
-    // console.log( 'comic',data[chapter].list[0].url )
+      slide = function slide() {
+    picidx = $items.find('.active').data('num') || 0;
+    $items.on('click', 'li', function () {
+      var $this = $(this);
+      $items.find('li').removeClass('active');
+      $this.addClass('active');
+      $slide.stop().animate({ scrollLeft: $this.data('pos') }, 400);
+      $page.find('img').attr('src', $this.find('img').attr('src'));
+    });
 
+    $('.btn-prev').on('click', function () {
+      if (picidx === 0) {
+        picidx = 0;
+      } else {
+        picidx = picidx - 1;
+      }
+      $items.find('[data-num=' + picidx + ']').trigger('click');
+    });
 
+    $('.btn-next').on('click', function () {
+      if (picidx === $item_len - 1) {
+        picidx = picidx;
+      } else {
+        picidx = picidx + 1;
+      }
+      $items.find('[data-num=' + picidx + ']').trigger('click');
+    });
+  },
+      creatPage = function creatPage(data) {
+
+    mandy.view.comicviewer.init(data, 0);
     $page = $layout.find('.m-page');
     $slide = $layout.find('.m-slide .items');
     $items = $slide.find('ul');
+    $item_len = data.comic[0].list.length;
 
-    data[chapter].list.map(function (e, i) {
-      mandy.model.comicviewer.imgLoad(e.url, slideEvent);
+    data.comic[0].list.map(function (e, i) {
+      imgLoad(e.url, i);
     });
 
-    // data[chapter].list[i].width = this.width;
-    // data[chapter].list[i].height = this.height;
-
-
-    // $item = $slide.find('li');
-    // $item_len = $item.length;
-
-    // $item.width( parseInt( $slide.outerWidth() / 6 ,10) );
-    // $slide.height( $item.outerHeight() + 40 );
-    // $slide.height( $item.find('img').height()  )
-
+    slide();
   },
       init = function init() {
 
-    mandy.model.comicviewer.getData({
-      data: {
-        chapter: 0,
-        pg: 0
-      },
-      success: creatPage
+    mandy.model.comicviewer.getData(function (data) {
+      console.log(data);
+      creatPage(data);
     });
   };
   return {
-    init: init,
-    slide: slide
+    init: init
   };
 }(jQuery);
 
